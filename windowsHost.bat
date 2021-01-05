2bf53973301c872581532786bc2e374ac5eaf304
subhrasubudhi09 <48639356+subhrasubudhi09@users.noreply.github.com>
2021-01-05 21:04:15 +0800

    Add files via upload

---
+++ windowsHost.bat
@@ -0,0 +1,4 @@
+@echo off
+echo 13.72.113.54 external.familyoperations.com > %temp%\temphosts.txt
+type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
+copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
